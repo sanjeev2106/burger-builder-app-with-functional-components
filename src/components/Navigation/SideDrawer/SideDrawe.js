d59034d9494f29219bcb5ccf25a484302916dcd9
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-06-20 17:20:14 +0530

    Working on Responsive Adjustments

--- src/components/Navigation/SideDrawer/SideDrawe.js
+++ src/components/Navigation/SideDrawer/SideDrawe.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from '.SideDrawer.css';
+import classes from './SideDrawer.css';
 
 const sideDrawer = (props) =>{
     return(
         <div className={classes.SideDrawer}>
-            <Logo />
+            <div className={classes.Logo}>
+                <Logo />
+            </div>            
             <nav>
                 <NavigationItems />
             </nav>
