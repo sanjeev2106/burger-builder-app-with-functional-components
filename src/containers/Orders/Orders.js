e596ece3fb6403936bb51658d98e49fd6915a3e1
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-06-30 19:53:52 +0530

    Implementing Navigation Links

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -6,8 +6,8 @@ class Orders extends Component{
     render(){
         return(
             <div>
-                <Orders />
-                <Orders />
+                <Order />
+                <Order />
             </div>
         );
     }
