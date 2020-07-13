443752bdd47900ac376463ec695bffa5e630ef10
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-07-13 11:10:42 +0530

    Removing console.log()s

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,8 +20,7 @@ class BurgerBuilder extends Component {
         purchasing: false
     }
 
-    componentDidMount () {
-        console.log(this.props); 
+    componentDidMount () {        
         this.props.onInitIngredients();
     }
 
